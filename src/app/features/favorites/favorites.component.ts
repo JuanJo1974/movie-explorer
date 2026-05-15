4c912b2e7cb469dc9e7fa21ecc5ff85870e4516c
JuanJo <juanjoseft@icloud.com>
2026-05-15 15:36:53 +0200

    feat: implement Favorites feature with localStorage persistence

--- src/app/features/favorites/favorites.component.ts
+++ src/app/features/favorites/favorites.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FavoritesService } from '../../core/services/favorites.service';
+import { MovieCardComponent } from '../../shared/components/movie-card/movie-card.component';
+import { Movie } from '../../core/models/movie.model';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  template: '<p>Favorites</p>'
+  imports: [RouterLink, MovieCardComponent],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.scss'
 })
-export class FavoritesComponent {}
+export class FavoritesComponent {
+  readonly favoritesService = inject(FavoritesService);
+
+  readonly favorites = this.favoritesService.favorites;
+
+  onFavoriteToggled(movie: Movie): void {
+    this.favoritesService.remove(movie.id);
+  }
+}
