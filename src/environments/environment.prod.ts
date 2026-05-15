b4d1086f7ba4aad5aaa0a3f6d133ed937c8dbf54
JuanJo <juanjoseft@icloud.com>
2026-05-15 14:52:05 +0200

    feat: add environments, global styles, and placeholder SVG

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  tmdbToken: 'TU_TMDB_READ_ACCESS_TOKEN_AQUI'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  tmdbToken: 'TU_TMDB_READ_ACCESS_TOKEN_AQUI'
+};
