55f4b988f3944a1903ab99160e1b86d69ae81cc9
JuanJo <juanjoseft@icloud.com>
2026-05-15 14:52:17 +0200

    feat: add core TypeScript models

---
+++ src/app/core/models/movie.model.ts
@@ -0,0 +1,44 @@
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  vote_average: number;
+  vote_count: number;
+  release_date: string;
+  genre_ids: number[];
+}
+
+export interface MovieDetail extends Movie {
+  runtime: number;
+  genres: Genre[];
+  tagline: string;
+}
+
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+}
+
+export interface Credits {
+  cast: Cast[];
+}
+
+export interface MovieListResponse {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
+
+export interface GenreListResponse {
+  genres: Genre[];
+}
